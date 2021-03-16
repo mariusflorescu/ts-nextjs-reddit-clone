447d75aeb4c04b97c3584467327463bd147f1c28
Marius Florescu <mariusflorescu@Mariuss-MacBook-Pro.local>
2021-03-16 21:54:13 +0200

    Refactor: created a post card

---
+++ client/components/Post.tsx
@@ -0,0 +1,72 @@
+import Link from 'next/link'
+import { Fragment } from 'react'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+import {Post} from '../types';
+
+dayjs.extend(relativeTime);
+
+interface IProps {
+  postKey: string;
+  data: Post;
+}
+
+export const PostCard : React.FC<IProps> = ({postKey,data}) =>{
+  return (
+    <div key={postKey} className="flex bg-white rounded">
+      <div className="w-10 text-center bg-gray-100 rounded-l">V</div>
+      <div className="w-full p-2 rounded-r">
+        <div className="flex items-center">
+          <Link href={`/r/${data.subName}`}>
+            <Fragment>
+              <img src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y" className="w-6 h-6 mr-1 rounded-full cursor-pointer"/>
+              <a className="text-xs font-bold cursor-pointer hover:underline">r/{data.subName}</a>
+          </Fragment>
+        </Link>
+        <p className="text-xs text-gray-500">
+          <span>•</span>
+          Posted by <Link href={`/u/${data.username}`}><a className="mx-1 hover:underline">/u/{data.username}</a></Link>
+          <Link href={data.url}>
+            <a className="mx-1 hover:underline">{dayjs(data.createdAt).fromNow()}</a>
+          </Link>
+        </p>
+      </div>
+
+      <Link href={data.url}>
+        <Fragment>
+          <a className="my-1 text-lg font-medium">{data.title}</a>  
+          {data.body && (
+            <p className="my-1 text-sm">{data.body}</p>
+          )}
+        </Fragment>
+      </Link>
+
+      <div className="flex">
+        <Link href={data.url}>
+          <a>
+            <div className="p-1 mr-2 space-x-1 text-xs text-gray-500 rounded cursor-pointer hover:bg-gray-200">
+              <i className="fas fa-comment-alt fa-xs"></i>
+              <span className="font-bold">30 comments</span>
+            </div>
+          </a>
+        </Link>
+
+        <a>
+            <div className="p-1 mr-2 space-x-1 text-xs text-gray-500 rounded cursor-pointer hover:bg-gray-200">
+              <i className="fas fa-bookmark"></i>
+              <span className="font-bold">30 comments</span>
+            </div>
+          </a>
+
+          <a>
+            <div className="p-1 mr-2 space-x-1 text-xs text-gray-500 rounded cursor-pointer hover:bg-gray-200">
+              <i className="fas fa-share fa-xs"></i>
+              <span className="font-bold">30 comments</span>
+            </div>
+          </a>
+      </div>
+    </div>
+  </div>
+  )
+}
